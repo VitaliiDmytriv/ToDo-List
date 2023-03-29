6182673de695c8580c072a94ee4a440bad95962f
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-03-29 20:53:09 +0300

    Add delete btn, add local storage, and refactor js code

--- app.js
+++ app.js
@@ -1,25 +1,77 @@
 const input = document.querySelector("#input");
 const btnAdd = document.querySelector(".btnAdd");
 const todoList = document.querySelector(".todoList");
-const arrOfTasks = [];
 
-btnAdd.addEventListener("click", renderTasks);
+const data = JSON.parse(window.localStorage.getItem("arrOfTasks"));
+// Перевіряє чи було щось в localStorage, якщо було, то записує у масив, якщо ні то створює масив
+const arrOfTasks = data === null ? [] : data;
+
+// Рендерить таски якщо були в local storage
+renderTasks();
+
+// Зберігає масив в LocalStorage
+function saveInStorage(arr, name) {
+    window.localStorage.setItem(name, JSON.stringify(arr));
+}
+
+// Отримує значення з input
+function getValueFromInput() {
+    const value = input.value;
+
+    // Якщо поле в інпуті не пусте то =>
+    if (/^.+$/.test(value)) {
+        arrOfTasks.push(value);
+
+        saveInStorage(arrOfTasks, "arrOfTasks");
+
+        // очищає інпут від попереднього тексту
+        input.value = "";
+
+        renderTasks();
+    }
+}
+
+// Рендерить таски
+function renderTasks() {
+    // Проходить методом map по масиві тасків і записує їх у тегах li
+    const result = arrOfTasks.map((el, i) => {
+        return `<li 
+                    data-id="${i + 1}."
+                >
+                    ${el}
+                    <button class = 'btnDelete'>
+                        <i class="fa-solid fa-x"></i>
+                    </button>
+                </li>`;
+    });
+
+    // ul лист встановлює html розмітку дочірніх елементів
+    todoList.innerHTML = result.join("");
+}
+
+// Додає таск
+btnAdd.addEventListener("click", getValueFromInput);
+
+// Додає таск нажиманням на enter
 document.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-        renderTasks();
+        getValueFromInput();
     }
 });
 
-function renderTasks() {
-    const value = input.value;
-    if (/^.+$/.test(value)) {
-        arrOfTasks.push(value);
-        input.value = "";
+// Видаляє таск
+todoList.addEventListener("click", (event) => {
+    const { target } = event;
 
-        const result = arrOfTasks.map((el, i) => {
-            return `<li data-id="${i + 1}.">${el}</li>`;
-        });
+    if (target.tagName === "BUTTON" || target.tagName === "I") {
+        // Отримує id  li елементу яке клікнули
+        const id = parseInt(target.parentElement.getAttribute("data-id"));
+        // Видаляє з масиву цей елемент за допомого id
+        arrOfTasks.splice(id - 1, 1);
 
-        todoList.innerHTML = result.join("");
+        // Оновлює дані в localStorage після видалення
+        saveInStorage(arrOfTasks, "arrOfTasks");
+        // Заново рендерить таски
+        renderTasks();
     }
-}
+});
