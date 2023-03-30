79503ac65912a84e7d84f0e0b31bdce3d12013b5
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-03-30 11:34:17 +0300

    Add style for checked li, refactor code js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#input");
 const btnAdd = document.querySelector(".btnAdd");
 const todoList = document.querySelector(".todoList");
+const checkBox = document.querySelector(".check");
 
 const data = JSON.parse(window.localStorage.getItem("arrOfTasks"));
 // Перевіряє чи було щось в localStorage, якщо було, то записує у масив, якщо ні то створює масив
@@ -20,7 +21,13 @@ function getValueFromInput() {
 
     // Якщо поле в інпуті не пусте то =>
     if (/^.+$/.test(value)) {
-        arrOfTasks.push(value);
+        // Створюємо об'єкт
+        const obj = {
+            value,
+            isChecked: false,
+        };
+
+        arrOfTasks.push(obj);
 
         // Оновлюємо localStorage
         saveInStorage(arrOfTasks, "arrOfTasks");
@@ -35,13 +42,13 @@ function getValueFromInput() {
 function renderTasks() {
     // Проходить методом map по масиві тасків і записує їх у тегах li
     const result = arrOfTasks.map((el, i) => {
-        return `<li 
-                    data-id="${i + 1}."
+        return `<li class = '${el.isChecked ? "active" : ""} '
+                    data-id="${i}"
                 >
                     <span class="check"></span>
-                    ${el}
-                    <button class = 'btnDelete'>
-                        <i class="fa-solid fa-x"></i>
+                    ${el.value}
+                    <button data-id = '${i}' class = 'btnDelete'>
+                        <i class="fa-solid fa-xmark"></i>
                     </button>
                 </li>`;
     });
@@ -66,13 +73,28 @@ todoList.addEventListener("click", (event) => {
 
     if (target.tagName === "BUTTON" || target.tagName === "I") {
         // Отримує id  li елементу яке клікнули
-        const id = parseInt(target.parentElement.getAttribute("data-id"));
+        const id = target.parentElement.getAttribute("data-id");
+
         // Видаляє з масиву цей елемент за допомого id
-        arrOfTasks.splice(id - 1, 1);
+        arrOfTasks.splice(id, 1);
 
         // Оновлює дані в localStorage після видалення
         saveInStorage(arrOfTasks, "arrOfTasks");
+
         // Заново рендерить таски
         renderTasks();
+    } else if (target.className === "check") {
+        // Отримуємо li в якому було натиснуто на check
+        const parent = target.parentElement;
+
+        // Отримуємо id цього li
+        const id = parent.getAttribute("data-id");
+
+        // Змінюємо значення isCheck на протилежне і збрерігаємо його у localStorage
+        arrOfTasks[id].isChecked = !arrOfTasks[id].isChecked;
+        saveInStorage(arrOfTasks, "arrOfTasks");
+
+        // Додаємо toggle класу
+        parent.classList.toggle("active");
     }
 });
